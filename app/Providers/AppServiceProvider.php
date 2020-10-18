82ab596ddf06deff90fde62b849f75c0a0775f0e
alfred <red.yamato@gmail.com>
2020-10-18 18:18:26 +0800

    add https as forcescheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if(env('APP_ENV') != 'local')
+        {
+            URL::forceScheme('https');
+        }
     }
 }
